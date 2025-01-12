14f93f3595e96f4cb914328360088103862b23ca
ujinsim <syj11243@naver.com>
2025-01-12 15:38:33 +0900

    chore : tailwind css 스토리북 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,10 @@ import { colors } from './src/shared/ui/colors/colors';
 
 export default {
   content: [
-    './app/**.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './stories/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     colors,
