36c2a48cc79b7cef8a82eb20057bc881bb36bda6
ujinsim <syj11243@naver.com>
2025-01-12 15:41:04 +0900

    feat: Storybook에 라우터 및 전역 CSS 적용 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import { withRouter } from 'storybook-addon-react-router-v6';
+import '../src/app/style/globals.css';
 
 const preview: Preview = {
   parameters: {
@@ -8,7 +10,11 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    nextjs: {
+      appDirectory: true,
+    },
   },
+  decorators: withRouter,
 };
 
 export default preview;
