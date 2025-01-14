5f1b01eda99957867d60f6ca6ae6b6c4c385fba1
keemsebeen <sebin0580@gmail.com>
2025-01-14 23:39:05 +0900

    chore : tailwind 경로 지정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,14 +4,12 @@ import { colors } from './src/shared/ui/colors/colors';
 
 export default {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors,
-    },
+    colors,
   },
   plugins: [],
 } satisfies Config;
