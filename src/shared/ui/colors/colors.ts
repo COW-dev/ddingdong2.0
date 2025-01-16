13bbb06876d2c59346de74b4132a54e0b6d5ecc5
keemsebeen <sebin0580@gmail.com>
2025-01-16 17:55:01 +0900

    style : color 색상 추가

--- src/shared/ui/colors/colors.ts
+++ src/shared/ui/colors/colors.ts
@@ -1,9 +1,10 @@
 export const colors = {
   primary: {
     50: '#EFF6FF',
-    100: '#dbeafe',
-    200: '#3b82f6',
-    300: '#2563eb',
+    100: '#DBEAFE',
+    200: '#BFDBFE',
+    300: '#3b82f6',
+    400: '#2563eb',
   },
   gray: {
     50: '#F9FAFB',
@@ -15,13 +16,16 @@ export const colors = {
     600: '#1f2937',
   },
   red: {
-    50: '#fef2f2',
-    100: '#f87171',
-    200: '#EF4444',
+    50: '#FEF2F2',
+    100: '#FEE2E2',
+    200: '#F87171',
+    300: '#EF4444',
   },
   green: {
     50: '#dcfce7',
-    100: '#22c55e',
+    100: '#BBF7D0',
+    200: '#22c55e',
+    300: '#16A34A',
   },
   emerald: '#10b981',
   orange: '#f97316',
