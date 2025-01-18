4291f9d1ff545c2e95f626f66a7123ca5439fa08
yougyung <youkyeongs@naver.com>
2025-01-18 23:00:47 +0900

    feat: tabs storybook 생성

--- src/shared/ui/Tabs/TabNav.tsx
+++ src/shared/ui/Tabs/TabNav.tsx
@@ -10,19 +10,16 @@ export function TabNav() {
   const { activeLabel, setActiveLabel, labels } = content;
 
   return (
-    <nav className="flex w-full items-center justify-between border-b border-gray-200 sm:block">
+    <nav className="flex w-full">
       {labels.map((label, index) => (
         <button
           key={index}
           onClick={() => setActiveLabel(label)}
-          className={cn(
-            `shrink-0 border-b-2 pb-4 text-base font-medium md:text-xl md:font-semibold ${
-              activeLabel === label
-                ? 'text-blue-500 border-primary-200'
-                : 'border-transparent hover:border-blue-500 hover:text-blue-500 text-gray-500'
-            }`,
-            `w-1/${labels.length}`
-          )}
+          className={`p-3 font-semibold w-1/${labels.length} ${
+            activeLabel === label
+              ? 'border-b-2 border-primary-300 text-primary-300 hover:bg-primary-50'
+              : 'text-gray-500 hover:bg-gray-50'
+          } md:p-4 md:text-xl`}
         >
           {label}
         </button>
