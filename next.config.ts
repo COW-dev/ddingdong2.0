02bced61f821fd2c0c7db807ba41fa37b8a98ef8
keemsebeen <sebin0580@gmail.com>
2025-01-20 21:06:25 +0900

    chore : svgr/webpack install

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+import path from 'path';
+
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, './src');
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 export { nextConfig };
