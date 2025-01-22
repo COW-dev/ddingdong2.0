4970f2a9f91ae3b5064d4a09cab3f16d249b33a1
yougyung <youkyeongs@naver.com>
2025-01-23 01:04:28 +0900

    refactor: 분리된 TabsNav 병합

--- src/shared/ui/Tabs/Tabs.context.ts
+++ src/shared/ui/Tabs/Tabs.context.ts
@@ -2,8 +2,6 @@ import { createContext } from 'react';
 
 type TabsContextType = {
   activeLabel: string;
-  setActiveLabel: (label: string) => void;
-  labels: string[];
 };
 
 export const TabsContext = createContext<TabsContextType | undefined>(undefined);
--- src/shared/ui/Tabs/TabsRoot.tsx
+++ src/shared/ui/Tabs/TabsRoot.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { Children, isValidElement, ReactElement, useMemo, useState } from 'react';
+import { motion } from 'framer-motion';
 
 import { TabsContext } from './Tabs.context';
-import { TabsNav } from './TabsNav';
 
 type Props = {
   children: React.ReactNode;
@@ -22,11 +22,30 @@ export function TabsRoot({ children, defaultIndex = 0 }: Props) {
   const [activeLabel, setActiveLabel] = useState<LabelType>(labels[defaultIndex]);
 
   return (
-    <TabsContext.Provider value={{ activeLabel, setActiveLabel, labels }}>
-      <div className="w-full">
-        <TabsNav />
-        {children}
-      </div>
+    <TabsContext.Provider value={{ activeLabel }}>
+      <nav className="flex w-full list-none items-center justify-center">
+        {labels.map((label) => (
+          <motion.button
+            key={label}
+            className={`grow p-3 font-semibold w-1/${labels.length} ${
+              activeLabel === label
+                ? 'translate-x-2 transform text-primary-300 hover:bg-primary-50'
+                : 'text-gray-500 hover:bg-gray-50'
+            } transition-all duration-300 ease-in-out md:p-4 md:text-xl`}
+            onClick={() => setActiveLabel(label)}
+          >
+            {label}
+            {label === activeLabel && (
+              <motion.div
+                className="absolute -bottom-0.5 left-0 right-0 h-0.5 bg-primary-300"
+                layoutId="underline"
+                id="underline"
+              />
+            )}
+          </motion.button>
+        ))}
+      </nav>
+      {children}
     </TabsContext.Provider>
   );
 }
