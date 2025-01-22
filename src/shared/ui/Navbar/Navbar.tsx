07ab7a27bfed511d4e427640cef37dc5bf4048a2
ujinsim <syj11243@naver.com>
2025-01-23 02:05:26 +0900

    feat : 아이콘 추가

--- src/shared/ui/Navbar/Navbar.tsx
+++ src/shared/ui/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
 
+import { Icon } from '../Icon';
 import { Title1 } from '../Typography/Typography';
 
 type NavbarProps = {
@@ -16,22 +17,8 @@ export default function Navbar({ title }: NavbarProps) {
       className="flex cursor-pointer flex-row items-center gap-4"
       onClick={() => router.back()}
     >
-      <svg
-        width="13"
-        height="21"
-        viewBox="0 0 13 21"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="stroke-gray-600"
-      >
-        <path
-          d="M10.9609 1.50024L2.75643 9.70475C2.31709 10.1441 2.31709 10.8564 2.75643 11.2957L10.9609 19.5002"
-          stroke="#4B5563"
-          strokeWidth="3"
-          strokeLinecap="round"
-        />
-      </svg>
-      <Title1 color="gray">{title}</Title1>
+      <Icon name="arrowLeft" />
+      <Title1 className="text-gray-400">{title}</Title1>
     </button>
   );
 }
---
+++ src/shared/ui/assets/index.tsx
@@ -0,0 +1,37 @@
+import Add from './add.svg';
+import ArrowDown from './arrow-down.svg';
+import ArrowLeft from './arrow-left.svg';
+import ArrowUp from './arrow-up.svg';
+import Check from './check.svg';
+import Close from './close.svg';
+import DownLoad from './download.svg';
+import Etc from './etc.svg';
+import Insta from './instagram.svg';
+import List from './list.svg';
+import New from './new.svg';
+import Pin from './pin.svg';
+import Trash from './trash.svg';
+import Write from './write.svg';
+import Youtube from './youtube.svg';
+
+export const Icons = {
+  add: Add,
+  arrowDown: ArrowDown,
+  arrowUp: ArrowUp,
+  check: Check,
+  close: Close,
+  download: DownLoad,
+  etc: Etc,
+  list: List,
+  new: New,
+  pin: Pin,
+  trash: Trash,
+  write: Write,
+  arrowLeft: ArrowLeft,
+
+  instagram: Insta,
+  youtube: Youtube,
+};
+
+export type IconName = keyof typeof Icons;
+export const iconNames = Object.keys(Icons) as IconName[];
