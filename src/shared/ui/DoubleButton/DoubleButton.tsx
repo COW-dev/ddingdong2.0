efaebd7da8e1e4dac47203198293ccd5e9bcdcda
yougyung <youkyeongs@naver.com>
2025-01-23 08:30:55 +0900

    refactor: ...props 지원 적용

--- src/shared/ui/DoubleButton/DoubleButton.tsx
+++ src/shared/ui/DoubleButton/DoubleButton.tsx
@@ -1,15 +1,21 @@
 import React, { ReactNode } from 'react';
 
 export type Props = {
+  /**
+   * left child of the of the DoubleButton
+   */
   left: ReactNode;
+  /**
+   * right child of the of the DoubleButton
+   */
   right: ReactNode;
-};
+} & React.HTMLAttributes<HTMLDivElement>;
 
-export function DoubleButton({ left, right }: Props) {
+export function DoubleButton({ left, right, ...props }: Props) {
   return (
-    <div className="flex items-center justify-center gap-2">
-      {left}
-      {right}
+    <div className="flex items-center justify-center" {...props}>
+      <div>{left}</div>
+      <div> {right}</div>
     </div>
   );
 }
