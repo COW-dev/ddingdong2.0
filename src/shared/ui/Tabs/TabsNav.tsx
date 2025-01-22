2a914f0b14e752ce846ac26054e3953274cf2d9d
yougyung <youkyeongs@naver.com>
2025-01-23 00:38:34 +0900

    feat: framer animation 적용

--- src/shared/ui/Tabs/TabsNav.tsx
+++ src/shared/ui/Tabs/TabsNav.tsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { motion } from 'framer-motion';
 
 import { TabsContext } from './Tabs.context';
 
@@ -8,19 +9,26 @@ export function TabsNav() {
   const { activeLabel, setActiveLabel, labels } = content;
 
   return (
-    <nav className="flex w-full">
-      {labels.map((label, index) => (
-        <button
-          key={index}
-          onClick={() => setActiveLabel(label)}
-          className={`grow border-b-2 border-primary-300 p-3 font-semibold w-1/${labels.length} ${
+    <nav className="flex w-full list-none items-center justify-center">
+      {labels.map((label) => (
+        <motion.button
+          key={label}
+          className={`grow p-3 font-semibold w-1/${labels.length} ${
             activeLabel === label
-              ? 'text-primary-300 hover:bg-primary-50'
-              : 'border-opacity-0 text-gray-500 hover:bg-gray-50'
-          } md:p-4 md:text-xl`}
+              ? 'translate-x-2 transform text-primary-300 hover:bg-primary-50'
+              : 'text-gray-500 hover:bg-gray-50'
+          } transition-all duration-300 ease-in-out md:p-4 md:text-xl`}
+          onClick={() => setActiveLabel(label)}
         >
           {label}
-        </button>
+          {label === activeLabel && (
+            <motion.div
+              className="absolute -bottom-0.5 left-0 right-0 h-0.5 bg-primary-300"
+              layoutId="underline"
+              id="underline"
+            />
+          )}
+        </motion.button>
       ))}
     </nav>
   );
