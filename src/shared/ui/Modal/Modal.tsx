e2db79244642c27c651af279ef8d7ee8a31a5eb2
ujinsim <syj11243@naver.com>
2025-01-23 16:26:37 +0900

    feat : framer-motion 적용

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import useOutsideClick from './useOutsideClick';
 
@@ -13,18 +14,37 @@ type ModalProps = {
 
 export default function Modal({ isOpen, closeModal, modalRef, children }: ModalProps) {
   useOutsideClick(modalRef, closeModal);
-  if (!isOpen) return null;
 
   return (
     <ModalPortal>
-      <div ref={modalRef} className="fixed z-30 flex items-center justify-center">
-        <div
-          className="flex max-w-4xl flex-col items-center rounded-xl bg-white p-10 text-4xl text-black shadow-xl md:h-[70%]"
-          onClick={(e) => e.stopPropagation()}
-        >
-          <div className="flex flex-col gap-8 text-center">{children}</div>
-        </div>
-      </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="fixed inset-0 z-30 flex items-center justify-center"
+          >
+            <div
+              ref={modalRef}
+              onClick={(e) => e.stopPropagation()}
+              className="flex items-center justify-center"
+            >
+              <motion.div
+                className="w-full max-w-4xl flex-col items-center rounded-xl bg-white p-10 text-4xl text-black shadow-xl md:h-[70%]"
+                onClick={(e) => e.stopPropagation()}
+                initial={{ scale: 0.8 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0.8 }}
+                transition={{ duration: 0.3 }}
+              >
+                <div className="flex flex-col gap-8 text-center">{children}</div>
+              </motion.div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </ModalPortal>
   );
 }
