4f49357b9e3ff49fb667cded1de701b9b1110ddf
yougyung <youkyeongs@naver.com>
2025-01-27 14:41:53 +0900

    fix: 불필요한 style 제거

--- src/shared/ui/Switch/Switch.tsx
+++ src/shared/ui/Switch/Switch.tsx
@@ -14,10 +14,10 @@ export function Switch({ color = 'primary', ...props }: Props) {
   return (
     <SwitchPrimitives.Root
       {...props}
-      className="group relative flex h-3.5 w-7 items-center rounded-full shadow-lg data-[state=checked]:bg-[var(--switch-color)] data-[state=unchecked]:bg-gray-500"
+      className="group flex h-3.5 w-7 items-center rounded-full shadow-lg data-[state=checked]:bg-[var(--switch-color)] data-[state=unchecked]:bg-gray-500"
       style={color && ({ '--switch-color': COLORS[color] } as React.CSSProperties)}
     >
-      <SwitchPrimitives.Thumb className="h-5 w-5 -translate-x-1.5 rounded-full bg-white shadow-inner duration-100 group-data-[state=checked]:translate-x-3.5" />
+      <SwitchPrimitives.Thumb className="h-5 w-5 -translate-x-1.5 rounded-full bg-white shadow-inner drop-shadow-md duration-100 group-data-[state=checked]:translate-x-3.5" />
     </SwitchPrimitives.Root>
   );
 }
