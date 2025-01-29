4e8a82e0b8ac209d0092ae99d1ffeb6d99dcd37e
ujinsim <syj11243@naver.com>
2025-01-29 11:01:47 +0900

    fix : NavBack 이름 변경

---
+++ src/shared/ui/NavBack/NavBack.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import NavBack from './NavBack';
+
+const meta: Meta<typeof NavBack> = {
+  title: 'components/common/NavBack',
+  component: NavBack,
+  decorators: [
+    (Story) => (
+      <div style={{ width: 400, backgroundColor: 'white', padding: 20 }}>
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+
+    docs: {
+      description: {
+        story: '클릭시 페이지 뒤로가기 이벤트를 발생시키는 컴포넌트입니다.',
+      },
+    },
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof NavBack>;
+
+export const Default: Story = {
+  args: {
+    title: 'NavBack 예시입니다',
+  },
+};
---
+++ src/shared/ui/NavBack/NavBack.tsx
@@ -0,0 +1,23 @@
+'use client';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+import { Icon } from '../Icon';
+
+type NavBackProps = {
+  title: string;
+};
+
+export default function NavBack({ title }: NavBackProps) {
+  const router = useRouter();
+
+  return (
+    <button
+      className="flex cursor-pointer flex-row items-center gap-2 align-middle"
+      onClick={() => router.back()}
+    >
+      <Icon name="navbarArrow" />
+      <p className="text-2xl font-bold text-gray-600">{title}</p>
+    </button>
+  );
+}
