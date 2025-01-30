88bc0a73d79ce44d1098a2852d87ad1ede05d66f
ujinsim <syj11243@naver.com>
2025-01-30 16:06:19 +0900

    update : 스토리북 설정 변경

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -1,17 +1,23 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Modal from './Modal';
+import { Modal } from './Modal';
 
 export default {
   title: 'components/common/Modal',
   component: Modal,
+  argTypes: {
+    mode: {
+      control: { type: 'select' },
+      options: ['wait', 'sync', 'popLayout'],
+      defaultValue: 'sync',
+    },
+  },
 } as Meta<typeof Modal>;
 
 export const Basic: StoryObj<typeof Modal> = {
   render: (args) => {
     const [isOpen, setIsOpen] = useState(false);
-    const modalRef = useRef<HTMLDivElement>(null);
 
     const openModal = (e: React.MouseEvent<HTMLButtonElement>) => {
       e.stopPropagation();
@@ -23,22 +29,16 @@ export const Basic: StoryObj<typeof Modal> = {
     };
 
     return (
-      <div>
-        <button
-          className="rounded-md bg-primary-300 p-3 font-semibold text-white"
-          onClick={openModal}
-        >
-          모달이 열립니다
+      <div className="flex flex-col items-center gap-4">
+        <button className="rounded-md bg-primary-300 px-4 py-2 text-white" onClick={openModal}>
+          모달 열기
         </button>
 
-        <Modal isOpen={isOpen} closeModal={closeModal} {...args}>
-          <div className="flex flex-col items-center gap-4 rounded-lg bg-white p-6">
-            <div className="p-2 text-2xl font-semibold">dding-dong 모달입니다</div>
-            <button
-              onClick={closeModal}
-              className="rounded-lg bg-red-200 px-4 py-2 font-bold text-white"
-            >
-              모달닫기
+        <Modal isOpen={isOpen} closeModal={closeModal} mode="wait">
+          <div className="flex flex-col items-center gap-4 rounded-lg bg-white p-6 shadow-sm">
+            <div className="p-2 text-2xl font-semibold">딩동 모달입니다.</div>
+            <button onClick={closeModal} className="rounded-lg bg-red-200 px-4 py-2 text-white">
+              모달 닫기
             </button>
           </div>
         </Modal>
