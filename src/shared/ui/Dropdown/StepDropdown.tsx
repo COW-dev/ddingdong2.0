a6cc4ec1916aaec94f572ddfb322625fb6ceaf2c
ujinsim <syj11243@naver.com>
2025-02-02 00:59:20 +0900

    feat : Step Dropdown 컴포넌트 제작

---
+++ src/shared/ui/Dropdown/StepDropdown.tsx
@@ -0,0 +1,56 @@
+'use client';
+import React, { useState } from 'react';
+
+import { Icon } from '../Icon';
+
+type Props = {
+  contents: { [key: string]: string[] };
+};
+
+export function StepDropdown({ contents }: Props) {
+  const allItems = Object.entries(contents).flatMap(([category, items]) =>
+    items.map((item) => ({ category, item }))
+  );
+
+  const [selectedContent, setSelectedContent] = useState(allItems[0]?.item || '');
+  const [openDropdown, setOpenDropdown] = useState(false);
+
+  return (
+    <div
+      className={`md:w-82 fixed w-72 cursor-pointer rounded-lg border bg-white text-start text-base font-semibold text-gray-400 md:text-lg ${openDropdown ? '' : 'border-gray-100'}`}
+    >
+      <div
+        className={`flex justify-between px-5 py-2 hover:bg-gray-100 md:py-3`}
+        onClick={() => setOpenDropdown(!openDropdown)}
+      >
+        {selectedContent}
+        {openDropdown ? <Icon name="arrowUp" /> : <Icon name="arrowDown" />}
+      </div>
+
+      {openDropdown && (
+        <div className="flex flex-col">
+          {Object.entries(contents).map(([category, items], categoryIndex) => (
+            <div key={categoryIndex} className="flex flex-col">
+              <div className="cursor-default border-t-2 border-gray-100 px-5 py-2 font-semibold text-gray-300">
+                {category}
+              </div>
+
+              {items.map((item, key) => (
+                <div
+                  key={key}
+                  className="cursor-pointer px-5 py-2 last:border-none hover:bg-gray-100 hover:last:rounded-b md:py-3"
+                  onClick={() => {
+                    setSelectedContent(item);
+                    setOpenDropdown(false);
+                  }}
+                >
+                  {item}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
