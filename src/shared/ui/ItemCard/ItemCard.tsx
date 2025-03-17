eef0e41ac6282f27e96cae88c16835446f2e5239
ujinsim <syj11243@naver.com>
2025-03-17 18:43:55 +0900

    fix : option onClick path로 변경

--- src/shared/ui/ItemCard/ItemCard.tsx
+++ src/shared/ui/ItemCard/ItemCard.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
-type Props = {
+export type Props = {
   children: React.ReactNode;
-  onClick: () => void;
   size: 'sm' | 'lg';
+  path: string;
 };
 
 const sizeClassesMap: Record<Props['size'], string> = {
-  sm: 'w-72 p-4',
-  lg: 'w-[330px] py-6 px-5',
+  sm: 'w-72 p-4 h-[99px]',
+  lg: 'w-[330px] py-6 px-5 md:h-[143px] h-[128px]',
 };
 
-export function ItemCard({ size, children, onClick }: Props) {
+export function ItemCard({ size, children, path }: Props) {
+  const router = useRouter();
+
   return (
     <div
-      onClick={onClick}
+      onClick={() => {
+        router.push(path);
+      }}
       className={`cursor-pointer rounded-xl border border-gray-200 hover:bg-gray-50 md:w-[343px] ${sizeClassesMap[size]}`}
     >
       {children}
