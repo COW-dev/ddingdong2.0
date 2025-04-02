d73c0a5ed829ae18a298aa007edb4e9a6f32190c
ujinsim <syj11243@naver.com>
2025-04-02 16:59:21 +0900

    style : text 반응형 추가

--- src/shared/ui/Select/OptionGroupName.tsx
+++ src/shared/ui/Select/OptionGroupName.tsx
@@ -6,5 +6,9 @@ type Props = {
 };
 
 export function OptionGroupName({ name }: Props) {
-  return <div className="border-b-2 border-gray-100 px-5 py-2 text-base text-gray-300">{name}</div>;
+  return (
+    <div className="border-b-2 border-gray-100 px-5 py-2 text-base text-gray-300 md:text-lg">
+      {name}
+    </div>
+  );
 }
