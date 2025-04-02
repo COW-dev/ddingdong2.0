b4daa258d610cd6f52c251201c74d00559215097
ujinsim <syj11243@naver.com>
2025-04-03 02:33:57 +0900

    fix : isOpen 조건부 렌더링 제거

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -14,19 +14,17 @@ type Props = {
 export function Modal({ isOpen, closeModal, children }: Props) {
   return (
     <Portal isOpen={isOpen}>
-      {isOpen && (
-        <motion.div
-          initial={MODAL_MOTION.initial}
-          animate={MODAL_MOTION.animate}
-          exit={MODAL_MOTION.exit}
-          className="fixed inset-0 z-30 flex w-full items-center justify-center"
-        >
-          <div className="absolute inset-0 bg-black bg-opacity-50" onClick={closeModal} />
-          <div className="relative z-40 items-center justify-center rounded-lg bg-white">
-            {children}
-          </div>
-        </motion.div>
-      )}
+      <motion.div
+        initial={MODAL_MOTION.initial}
+        animate={MODAL_MOTION.animate}
+        exit={MODAL_MOTION.exit}
+        className="fixed inset-0 z-30 flex w-full items-center justify-center"
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-50" onClick={closeModal} />
+        <div className="relative z-40 items-center justify-center rounded-lg bg-white">
+          {children}
+        </div>
+      </motion.div>
     </Portal>
   );
 }
