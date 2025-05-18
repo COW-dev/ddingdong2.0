7dfcbcf612337f2010a27ec4f11532f7597b159d
ujinsim <syj11243@naver.com>
2025-05-18 12:56:49 +0900

    feat : cva 적용

--- src/shared/ui/Select/Option.tsx
+++ src/shared/ui/Select/Option.tsx
@@ -1,35 +1,41 @@
 'use client';
 import React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/shared/lib/core';
 
 import { useSelectContext } from './Select.context';
 
+const optionVariants = cva(
+  'font-semibold text- cursor-pointer first:rounded-t-md last:rounded-b-md hover:bg-gray-50',
+  {
+    variants: {
+      size: {
+        md: 'px-3 py-1 text-sm',
+        lg: 'px-5 py-2 md:py-2.5',
+      },
+    },
+    defaultVariants: {
+      size: 'lg',
+    },
+  }
+);
+
 type Props = {
   id: string;
   name: string;
-};
+} & VariantProps<typeof optionVariants>;
 
-type Size = 'md' | 'lg';
-
-export function Option({ id, name }: Props) {
-  const { onSelect, size } = useSelectContext() as {
+export function Option({ id, name, size }: Props) {
+  const { onSelect, size: contextSize } = useSelectContext() as {
     onSelect: (arg: { id: string; name: string }) => void;
-    size: Size;
-  };
-
-  const sizeVariants = {
-    md: 'px-3 py-1 text-sm',
-    lg: 'px-5 py-2 md:py-2.5',
+    size: 'md' | 'lg';
   };
 
   return (
     <div
       onClick={() => onSelect({ id, name })}
-      className={cn(
-        'cursor-pointer first:rounded-t-md last:rounded-b-md hover:bg-gray-50',
-        sizeVariants[size]
-      )}
+      className={cn(optionVariants({ size: size || contextSize }))}
     >
       {name}
     </div>
--- src/shared/ui/Select/OptionList.tsx
+++ src/shared/ui/Select/OptionList.tsx
@@ -1,30 +1,32 @@
 'use client';
 import React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/shared/lib/core';
 
 import { useSelectContext } from './Select.context';
 
+const optionListVariants = cva(
+  'mt-1 max-h-60 overflow-y-auto rounded-md border border-gray-200 bg-white font-semibold text-gray-400',
+  {
+    variants: {
+      size: {
+        md: 'text-sm min-w-24',
+        lg: 'text-base md:text-lg',
+      },
+    },
+    defaultVariants: {
+      size: 'lg',
+    },
+  }
+);
+
 type Props = {
   children: React.ReactNode;
-};
+} & VariantProps<typeof optionListVariants>;
 
-export function OptionList({ children }: Props) {
-  const { size } = useSelectContext();
+export function OptionList({ children, size }: Props) {
+  const { size: contextSize } = useSelectContext();
 
-  const sizeVariants = {
-    md: 'text-sm min-w-24',
-    lg: 'text-base md:text-lg',
-  };
-
-  return (
-    <div
-      className={cn(
-        'mt-1 max-h-60 overflow-y-auto rounded-md border border-gray-200 bg-white font-semibold text-gray-400',
-        sizeVariants[size]
-      )}
-    >
-      {children}
-    </div>
-  );
+  return <div className={cn(optionListVariants({ size: size || contextSize }))}>{children}</div>;
 }
